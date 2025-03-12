35f56d1a6c8d8925ff45405b56945fcd9be2da0c
Edward Banner <edward.banner@gmail.com>
2025-03-12 14:27:26 -0400

    Get tokens.json from boto

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import os
 
-from google.oauth2.credentials import Credentials
 import googleapiclient.discovery
-
 from dotenv import load_dotenv
+from google.oauth2.credentials import Credentials
+
 load_dotenv()
 
+import json
 
-credentials = Credentials.from_authorized_user_file(
-    "tokens.json"
-)
+import boto3
+
+
+secrets_client = boto3.client("secretsmanager")
+
+def get_secret(secret_name, secret_key=None):
+    response = secrets_client.get_secret_value(SecretId=secret_name)
+    result = response['SecretString']
+    secrets = json.loads(result)
+
+    if secret_key:
+        return secrets[secret_key]
+    else:
+        return secrets
+
+
+tokens_json = get_secret('SaveYouTubeStream', 'tokens.json')
+tokens_dict = json.loads(tokens_json)
+
+credentials = Credentials.from_authorized_user_info(tokens_dict)
 
 youtube = googleapiclient.discovery.build('youtube', 'v3', credentials=credentials)
+
 
 def get_live_video_id():
     CHANNEL_ID = os.environ['CHANNEL_ID']
