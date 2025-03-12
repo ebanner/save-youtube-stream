2cafa3b2f88c314aa4e71426c1c45904d5128d04
Edward Banner <edward.banner@gmail.com>
2025-03-12 14:16:15 -0400

    Add to Watch Next playlist

--- main.py
+++ main.py
@@ -13,17 +13,47 @@ credentials = Credentials.from_authorized_user_file(
 
 youtube = googleapiclient.discovery.build('youtube', 'v3', credentials=credentials)
 
+def get_live_video_id():
+    CHANNEL_ID = os.environ['CHANNEL_ID']
 
-if __name__ == '__main__':
-    channel_id = os.environ['CHANNEL_ID']
     request = youtube.search().list(
         part="snippet",
-        channelId=channel_id,
+        channelId=CHANNEL_ID,
         eventType="live",
         type="video"
     )
     response = request.execute()
 
+    print(response)
+
     live_video_id = response["items"][0]['id']['videoId']
-    print(live_video_id)
+
+    return live_video_id
+
+
+def add_to_watch_next_playlist(live_video_id):
+    WATCH_NEXT_PLAYLIST_ID = os.environ['WATCH_NEXT_PLAYLIST_ID']
+
+    request = youtube.playlistItems().insert(
+        part="snippet",
+        body={
+            "snippet": {
+                "playlistId": WATCH_NEXT_PLAYLIST_ID,
+                "position": 0,
+                "resourceId": {
+                "kind": "youtube#video",
+                "videoId": live_video_id
+                }   
+            }   
+        }
+    )
+
+    response = request.execute()
+    print(response)
+    return response
+
+
+if __name__ == '__main__':
+    live_video_id = get_live_video_id()
+    add_to_watch_next_playlist(live_video_id)
 
